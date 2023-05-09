e683577d7cbc1ae23a5ff4ecd262ce2af249c67c
JSharpb <jordanchristopherbirdsong@gmail.com>
2023-05-09 14:31:55 -0500

    Merged php files and converted to html; separated styles from head

--- footer.php
+++
@@ -1,7 +0,0 @@
-    <?php wp_footer(); ?>
-    <footer>
-        <p>© 2023 Faithful Bundles LLC</p>
-        <a target="_blank" href="https://jbsong.pages.dev">Website designed by Jordan Birdsong</a>
-    </footer>
-</body>
-</html>
